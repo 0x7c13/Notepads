d47be6b96ce7844b698c996f654f71206e59ae56
Jackie Liu <jackil@microsoft.com>
2019-07-24 12:36:18 -0700

    Better naming
    Better naming

--- src/Notepads/Extensions/DiffViewer/SideBySideDiffViewer.xaml.cs
+++ src/Notepads/Extensions/DiffViewer/SideBySideDiffViewer.xaml.cs
@@ -4,21 +4,21 @@ namespace Notepads.Extensions.DiffViewer
     using System.Collections.Generic;
     using Windows.UI.Xaml.Controls;
 
-    public sealed partial class DiffViewer : Page, ISideBySideDiffViewer
+    public sealed partial class SideBySideDiffViewer : Page, ISideBySideDiffViewer
     {
         private readonly TextBoxDiffRenderer diffRenderer;
         private readonly ScrollViewerSynchronizer scrollSynchronizer;
 
-        public DiffViewer()
+        public SideBySideDiffViewer()
         {
             InitializeComponent();
             scrollSynchronizer = new ScrollViewerSynchronizer(new List<ScrollViewer> { LeftScroller, RightScroller });
             diffRenderer = new TextBoxDiffRenderer();
         }
 
-        public void RenderDiff(string originalContent, string newContent)
+        public void RenderDiff(string left, string right)
         {
-            var diffData = diffRenderer.GenerateDiffViewData(originalContent, newContent);
+            var diffData = diffRenderer.GenerateDiffViewData(left, right);
             var leftData = diffData.Item1;
             var rightData = diffData.Item2;
 
