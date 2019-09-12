c779622afb57a85f0be6c05c672015d1b50a61a4
Lucas Pinho B. Santos <5108881+pinholucas@users.noreply.github.com>
2019-09-12 05:29:36 -0300

    Initial UI implementation

---
+++ src/Notepads/Controls/GoTo/GoToEventArgs.cs
@@ -0,0 +1,15 @@
+﻿namespace Notepads.Controls.GoTo
+{
+    using System;
+
+    public class GoToEventArgs : EventArgs
+    {
+        public GoToEventArgs(string searchLine)
+        {
+            SearchLine = searchLine;
+        }
+
+        public string SearchLine { get; }
+
+    }
+}
