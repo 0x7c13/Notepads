bfe07d02853bccc54aea4311fb350a868da0569a
Jiaqi Liu <jackil@microsoft.com>
2020-03-03 01:48:19 -0800

    Shuts down app on "Class not registered (Exception from HRESULT: 0x80040154)" exception
    Shuts down app on "Class not registered (Exception from HRESULT: 0x80040154)" exception

--- src/Notepads/App.xaml.cs
+++ src/Notepads/App.xaml.cs
@@ -110,9 +110,15 @@
             Analytics.TrackEvent("OnUnhandledException", diagnosticInfo);
             Crashes.TrackError(e.Exception, diagnosticInfo);
 
-            // if you want to suppress and handle it manually, 
-            // otherwise app shuts down.
-            e.Handled = true;
+            if (e.Message.Contains("0x80040154", StringComparison.InvariantCultureIgnoreCase))
+            {
+                // To keep track of "Class not registered (Exception from HRESULT: 0x80040154)" exception
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;   
+            }
         }
 
         private static void OnUnobservedException(object sender, UnobservedTaskExceptionEventArgs e)
