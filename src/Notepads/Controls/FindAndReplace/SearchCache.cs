adb5ce9e49464d62e515b99c94326b5b8325d58a
Soumya Ranjan Mahunt <devsoumyamahunt@gmail.com>
2020-03-30 05:30:16 +0530

    WIP

---
+++ src/Notepads/Controls/FindAndReplace/SearchCache.cs
@@ -0,0 +1,21 @@
+﻿namespace Notepads.Controls.FindAndReplace
+{
+    public class SearchCache
+    {
+        public SearchCache(string searchText, bool matchCase = false, bool matchWholeWord = false, bool useRegex = false)
+        {
+            SearchText = searchText;
+            MatchCase = matchCase;
+            MatchWholeWord = matchWholeWord;
+            UseRegex = useRegex;
+        }
+
+        public string SearchText { get; }
+
+        public bool MatchCase { get; }
+
+        public bool MatchWholeWord { get; }
+
+        public bool UseRegex { get; }
+    }
+}
--- src/Notepads/Controls/TextEditor/TextEditor.xaml.cs
+++ src/Notepads/Controls/TextEditor/TextEditor.xaml.cs
@@ -36,25 +36,6 @@
         RenamedMovedOrDeleted
     }
 
-    public class SearchCache
-    {
-        public SearchCache(string searchText, bool matchCase = false, bool matchWholeWord = false, bool useRegex = false)
-        {
-            SearchText = searchText;
-            MatchCase = matchCase;
-            MatchWholeWord = matchWholeWord;
-            UseRegex = useRegex;
-        }
-
-        public string SearchText { get; }
-
-        public bool MatchCase { get; }
-
-        public bool MatchWholeWord { get; }
-
-        public bool UseRegex { get; }
-    }
-
     public sealed partial class TextEditor : ITextEditor
     {
         public new event RoutedEventHandler Loaded;
