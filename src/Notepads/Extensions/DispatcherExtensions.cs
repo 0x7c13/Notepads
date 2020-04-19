34867b17b18dfe4fce20af6b605404da5311f21d
Jiaqi Liu <jackil@microsoft.com>
2020-04-18 23:10:10 -0700

    Fix merge issues

--- src/Notepads/Extensions/DispatcherExtensions.cs
+++ src/Notepads/Extensions/DispatcherExtensions.cs
@@ -10,5 +10,28 @@
         {
             await dispatcher.RunAsync(CoreDispatcherPriority.Normal, handler);
         }
+
+        public static async Task<T> RunTaskAsync<T>(this CoreDispatcher dispatcher,
+            Func<Task<T>> func, CoreDispatcherPriority priority = CoreDispatcherPriority.Normal)
+        {
+            var taskCompletionSource = new TaskCompletionSource<T>();
+            await dispatcher.RunAsync(priority, async () =>
+            {
+                try
+                {
+                    taskCompletionSource.SetResult(await func());
+                }
+                catch (Exception ex)
+                {
+                    taskCompletionSource.SetException(ex);
+                }
+            });
+            return await taskCompletionSource.Task;
+        }
+
+        // There is no TaskCompletionSource<void> so we use a bool that we throw away.
+        public static async Task RunTaskAsync(this CoreDispatcher dispatcher,
+            Func<Task> func, CoreDispatcherPriority priority = CoreDispatcherPriority.Normal) =>
+            await RunTaskAsync(dispatcher, async () => { await func(); return false; }, priority);
     }
 }
