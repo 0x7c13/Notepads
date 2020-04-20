c50834a70b2db041f1ff9b6e4b64a0e6d8da7116
Soumya Ranjan Mahunt <devsoumyamahunt@gmail.com>
2020-04-20 19:53:34 +0530

    Made security improvement.

--- src/Notepads.Services/InteropConnectionService.cs
+++ src/Notepads.Services/InteropConnectionService.cs
@@ -18,14 +18,21 @@
             // Get a deferral so that the service isn't terminated.
             this.backgroundTaskDeferral = taskInstance.GetDeferral();
 
-            // Associate a cancellation handler with the background task.
-            taskInstance.Canceled += OnTaskCanceled;
-
             // Retrieve the app service connection and set up a listener for incoming app service requests.
             var details = taskInstance.TriggerDetails as AppServiceTriggerDetails;
-            appServiceConnection = details.AppServiceConnection;
-            appServiceConnections.Add(appServiceConnection);
-            appServiceConnection.RequestReceived += OnRequestReceived;
+            if(details.CallerPackageFamilyName == Package.Current.Id.FamilyName)
+            {
+                appServiceConnection = details.AppServiceConnection;
+                appServiceConnections.Add(appServiceConnection);
+                appServiceConnection.RequestReceived += OnRequestReceived;
+
+                // Associate a cancellation handler with the background task.
+                taskInstance.Canceled += OnTaskCanceled;
+            }
+            else
+            {
+                this.backgroundTaskDeferral.Complete();
+            }
         }
 
         private async void OnRequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
