c055d44edf4a8f66abc1e6cde2ad83c0ec037d3d
Soumya Ranjan Mahunt <devsoumyamahunt@gmail.com>
2020-05-16 08:51:25 +0530

    Implemented optional language change feature (#540)
    * Implemented optional language change feature for Internationalization and Localization contributors.

---
+++ src/Notepads/Utilities/LanguageUtility.cs
@@ -0,0 +1,40 @@
+﻿namespace Notepads.Utilities
+{
+    using System.Collections.Generic;
+    using System.Globalization;
+    using Windows.Globalization;
+    using Windows.UI.Xaml;
+
+    public class LanguageItem
+    {
+        private string _id;
+        public string ID
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+                _name = string.IsNullOrEmpty(value) ? "System Default" : new CultureInfo(value).NativeName;
+            }
+        }
+
+        private string _name;
+        public string Name { get => _name; }
+    }
+
+    public static class LanguageUtility
+    {
+        public static readonly string CurrentLanguageID = ApplicationLanguages.PrimaryLanguageOverride;
+        public static Visibility RestrtPromptVisibility = Visibility.Collapsed;
+
+        public static IReadOnlyCollection<LanguageItem> GetSupportedLanguageItems()
+        {
+            var supportedLanguageList = new List<LanguageItem>() { new LanguageItem() { ID = string.Empty } };
+            foreach (var languageId in ApplicationLanguages.ManifestLanguages)
+            {
+                supportedLanguageList.Add(new LanguageItem() { ID = languageId });
+            }
+            return supportedLanguageList;
+        }
+    }
+}
--- src/Notepads/Views/Settings/AdvancedSettingsPage.xaml.cs
+++ src/Notepads/Views/Settings/AdvancedSettingsPage.xaml.cs
@@ -1,11 +1,17 @@
 ﻿namespace Notepads.Views.Settings
 {
     using Notepads.Services;
+    using Notepads.Utilities;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Windows.Globalization;
     using Windows.UI.Xaml;
     using Windows.UI.Xaml.Controls;
 
     public sealed partial class AdvancedSettingsPage : Page
     {
+        private IReadOnlyCollection<LanguageItem> SupportedLanguages;
+
         public AdvancedSettingsPage()
         {
             InitializeComponent();
@@ -25,6 +31,14 @@
 
             AlwaysOpenNewWindowToggleSwitch.IsOn = AppSettingsService.AlwaysOpenNewWindow;
 
+#if DEBUG
+            SupportedLanguages = LanguageUtility.GetSupportedLanguageItems();
+            if (LanguagePreferenceSettingsPanel == null)
+            {
+                FindName("LanguagePreferenceSettingsPanel"); // Lazy loading
+            }
+            LanguagePicker.SelectedItem = SupportedLanguages.FirstOrDefault(language => language.ID == ApplicationLanguages.PrimaryLanguageOverride);
+#endif
             if (App.IsGameBarWidget)
             {
                 // these settings don't make sense for Game Bar, there can be only one
@@ -43,6 +57,9 @@
             ShowStatusBarToggleSwitch.Toggled += ShowStatusBarToggleSwitch_Toggled;
             EnableSessionSnapshotToggleSwitch.Toggled += EnableSessionBackupAndRestoreToggleSwitch_Toggled;
             AlwaysOpenNewWindowToggleSwitch.Toggled += AlwaysOpenNewWindowToggleSwitch_Toggled;
+#if DEBUG
+            LanguagePicker.SelectionChanged += LanguagePicker_SelectionChanged;
+#endif
         }
 
         private void AdvancedSettings_Unloaded(object sender, RoutedEventArgs e)
@@ -65,6 +82,22 @@
         private void AlwaysOpenNewWindowToggleSwitch_Toggled(object sender, RoutedEventArgs e)
         {
             AppSettingsService.AlwaysOpenNewWindow = AlwaysOpenNewWindowToggleSwitch.IsOn;
+        }
+
+        private void LanguagePicker_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedID = ((LanguageItem)LanguagePicker.SelectedItem).ID;
+
+            if (selectedID == LanguageUtility.CurrentLanguageID)
+            {
+                RestartPrompt.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                RestartPrompt.Visibility = Visibility.Visible;
+            }
+
+            ApplicationLanguages.PrimaryLanguageOverride = selectedID;
         }
     }
 }
