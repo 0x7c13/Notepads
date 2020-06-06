4b0e700f49e330133c278a8cc7e5cb7e8d2b85b0
Soumya Ranjan Mahunt <devsoumyamahunt@gmail.com>
2020-06-06 12:13:13 +0530

    code refactor.

--- src/Notepads/Controls/TextEditor/TextEditorCore.MoveText.cs
+++ src/Notepads/Controls/TextEditor/TextEditorCore.MoveText.cs
@@ -25,41 +25,7 @@
             var endLineFinalIndex = end - endColumn + lines[endLine - 1].Length +
                 (end > start && (end > document.Length || document[end - 1].Equals(RichEditBoxDefaultLineEnding)) ? 0 : 1);
 
-            var selectionMoveAmount = lines[startLine - 2].Length + 1;
-            var insertIndex = startLineInitialIndex - selectionMoveAmount;
-            var movingText = document.Substring(startLineInitialIndex, endLineFinalIndex - startLineInitialIndex);
-            var remainingContent = document.Remove(startLineInitialIndex, endLineFinalIndex - startLineInitialIndex);
-
-            if (insertIndex < 0)
-            {
-                insertIndex = 0;
-                remainingContent = RichEditBoxDefaultLineEnding + remainingContent;
-                movingText = movingText.Remove(0, 1);
-            }
-
-            Document.Selection.GetRect(PointOptions.Transform, out Windows.Foundation.Rect rect, out var _);
-            GetScrollViewerPosition(out var horizontalOffset, out var verticalOffset);
-            var wasSelectionInView = IsSelectionRectInView(rect, horizontalOffset, verticalOffset);
-
-            var newContent = remainingContent.Insert(insertIndex, movingText);
-            start -= selectionMoveAmount;
-            end -= selectionMoveAmount;
-            if (start < 0) start = 0;
-
-            Document.SetText(TextSetOptions.None, newContent);
-            Document.Selection.SetRange(start, end);
-
-            // After SetText() and SetRange(), RichEdit will scroll selection into view
-            // and change scroll viewer's position even if it was already in the viewport
-            // It is better to keep its original scroll position after changing the texts' position in this case
-            if (wasSelectionInView)
-            {
-                _contentScrollViewer.ChangeView(
-                    horizontalOffset,
-                    verticalOffset,
-                    zoomFactor: null,
-                    disableAnimation: true);
-            }
+            MoveLines(document, startLineInitialIndex, endLineFinalIndex, start, end, -lines[startLine - 2].Length - 1);
         }
 
         private void MoveTextDown()
@@ -82,12 +48,24 @@
             var endLineFinalIndex = end - endColumn + lines[endLine - 1].Length +
                 (end > start && document[end - 1].Equals(RichEditBoxDefaultLineEnding) ? 1 : 2);
 
-            var selectionMoveAmount = lines[endLine].Length + 1;
+            MoveLines(document, startLineInitialIndex, endLineFinalIndex, start, end, lines[endLine].Length + 1);
+        }
+
+        private void MoveLines(string document,
+            int startLineInitialIndex, int endLineFinalIndex,
+            int selectionStart, int selectionEnd, int selectionMoveAmount)
+        {
             var insertIndex = startLineInitialIndex + selectionMoveAmount;
             var movingText = document.Substring(startLineInitialIndex, endLineFinalIndex - startLineInitialIndex);
             var remainingContent = document.Remove(startLineInitialIndex, endLineFinalIndex - startLineInitialIndex);
 
-            if (insertIndex >= remainingContent.Length)
+            if (insertIndex < 0)
+            {
+                insertIndex = 0;
+                remainingContent = RichEditBoxDefaultLineEnding + remainingContent;
+                movingText = movingText.Remove(0, 1);
+            }
+            else if (insertIndex >= remainingContent.Length)
             {
                 remainingContent += RichEditBoxDefaultLineEnding;
                 movingText = movingText.Remove(movingText.Length - 1);
@@ -98,11 +76,12 @@
             var wasSelectionInView = IsSelectionRectInView(rect, horizontalOffset, verticalOffset);
 
             var newContent = remainingContent.Insert(insertIndex, movingText);
-            start += selectionMoveAmount;
-            end += selectionMoveAmount;
+            selectionStart += selectionMoveAmount;
+            selectionEnd += selectionMoveAmount;
+            if (selectionStart < 0) selectionStart = 0;
 
             Document.SetText(TextSetOptions.None, newContent);
-            Document.Selection.SetRange(start, end);
+            Document.Selection.SetRange(selectionStart, selectionEnd);
 
             // After SetText() and SetRange(), RichEdit will scroll selection into view
             // and change scroll viewer's position even if it was already in the viewport
@@ -128,8 +107,8 @@
             var movingWordIndexData = GetMovingWordsIndexData(document, start, end);
             var startIndex = movingWordIndexData.Item1;
             var endIndex = movingWordIndexData.Item2;
-            end = movingWordIndexData.Item3;
             if (startIndex <= 0 || startIndex >= endIndex) return;
+            end = movingWordIndexData.Item3;
 
             var replacedWordEndIndex = startIndex;
             while (replacedWordEndIndex > 0)
