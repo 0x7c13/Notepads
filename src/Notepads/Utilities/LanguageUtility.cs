d607435b9dd172fff6b110589763e1ef805a3a5c
Soumya Ranjan Mahunt <devsoumyamahunt@gmail.com>
2020-06-13 19:15:54 +0530

    Fix to show all supported languages.

--- src/Notepads/Utilities/LanguageUtility.cs
+++ src/Notepads/Utilities/LanguageUtility.cs
@@ -27,13 +27,39 @@
 
     public static class LanguageUtility
     {
+        private static readonly IReadOnlyList<string> _manifestLanguages = new List<string>()
+        {
+            "en-US",
+            "bg",
+            "cs", 
+            "de-CH",
+            "de-DE",
+            "es-ES", 
+            "fi",
+            "fr-FR", 
+            "hi", 
+            "hr-HR", 
+            "hu",
+            "it-IT",
+            "ja",
+            "ka",
+            "ko",
+            "or",
+            "pl",
+            "pt-BR",
+            "ru",
+            "tr",
+            "uk",
+            "zh-Hans-CN",
+            "zh-Hant-TW"
+        };
+
         public static readonly string CurrentLanguageID = ApplicationLanguages.PrimaryLanguageOverride;
 
         public static IReadOnlyCollection<LanguageItem> GetSupportedLanguageItems()
         {
             var supportedLanguageList = new List<LanguageItem>() { new LanguageItem() { ID = string.Empty } };
-            supportedLanguageList.AddRange(ApplicationLanguages.ManifestLanguages
-                .Select(languageId => new LanguageItem() { ID = languageId }));
+            supportedLanguageList.AddRange(_manifestLanguages.Select(languageId => new LanguageItem() { ID = languageId }));
             return supportedLanguageList;
         }
     }
