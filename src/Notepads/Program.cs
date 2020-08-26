9fc9345ad2305ed65d385ec02f2ad65840a9c024
Soumya Ranjan Mahunt <devsoumyamahunt@gmail.com>
2020-08-26 12:59:48 +0530

    Fixed building in debug mode.

--- src/Notepads/Program.cs
+++ src/Notepads/Program.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Linq;
     using System.Threading;
+    using System.Threading.Tasks;
     using Notepads.Services;
     using Notepads.Settings;
     using Windows.ApplicationModel;
