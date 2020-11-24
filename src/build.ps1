6d3c73a5d2e7fc2702898ebc73b893441ae29078
Soumya Ranjan Mahunt <devsoumyamahunt@gmail.com>
2020-11-24 15:15:59 +0530

    Resolved variable conflits.

--- src/build.ps1
+++ src/build.ps1
@@ -1,17 +1,17 @@
 param (
   [string]$project_dir = $(throw "-project_dir=<path to project> is required"),
-  [string]$event = "PostBuild"
+  [string]$target = "PostBuild"
 )
 
 $project_dir = Resolve-Path $project_dir.TrimEnd("\")
 $manifest_file = "${project_dir}\Package.appxmanifest"
 
 [xml]$manifest = Get-Content $manifest_file
-if ($event -eq "PreBuild") {
+if ($target -eq "PreBuild") {
     $manifest.Package.Identity.Name="Notepads"
     $manifest.Package.Applications.Application.VisualElements.DisplayName="Notepads"
     $manifest.Package.Applications.Application.Extensions.Extension.AppExecutionAlias.ExecutionAlias.Alias="Notepads.exe"
-} elseif ($event -eq "PostBuild") {
+} elseif ($target -eq "PostBuild") {
     $manifest.Package.Identity.Name="Notepads-Dev"
     $manifest.Package.Applications.Application.VisualElements.DisplayName="Notepads-Dev"
     $manifest.Package.Applications.Application.Extensions.Extension.AppExecutionAlias.ExecutionAlias.Alias="Notepads-Dev.exe"
