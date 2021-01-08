5970d3ee40a21ae9ab3651fe2f3411209ef18e12
Soumya Ranjan Mahunt <devsoumyamahunt@gmail.com>
2021-01-08 19:12:12 +0530

    Modified script to only apply for Debug and Release builds.

--- src/Notepads/build.ps1
+++ src/Notepads/build.ps1
@@ -1,5 +1,6 @@
 param (
   [string]$project_dir = $(throw "-project_dir=<path to project> is required"),
+  [string]$config = "Debug",
   [string]$target = "PostBuild"
 )
 
@@ -7,13 +8,27 @@ $project_dir = Resolve-Path $project_dir.TrimEnd("\")
 $manifest_file = "${project_dir}\Package.appxmanifest"
 
 [xml]$manifest = Get-Content $manifest_file
-if ($target -eq "PreBuild") {
-    $manifest.Package.Identity.Name="Notepads"
-    $manifest.Package.Applications.Application.VisualElements.DisplayName="Notepads"
-    $manifest.Package.Applications.Application.Extensions.Extension.AppExecutionAlias.ExecutionAlias.Alias="Notepads.exe"
-} elseif ($target -eq "PostBuild") {
-    $manifest.Package.Identity.Name="Notepads-Dev"
-    $manifest.Package.Applications.Application.VisualElements.DisplayName="Notepads-Dev"
-    $manifest.Package.Applications.Application.Extensions.Extension.AppExecutionAlias.ExecutionAlias.Alias="Notepads-Dev.exe"
+
+if ($config -eq "Debug" -Or $config -eq "Release") {
+    if ($target -eq "PreBuild") {
+        $manifest.Package.Identity.Name="Notepads-Dev"
+        $manifest.Package.Applications.Application.VisualElements.DisplayName="Notepads-Dev"
+        $manifest.Package.Applications.Application.Extensions.Extension.AppExecutionAlias.ExecutionAlias.Alias="Notepads-Dev.exe"
+    } elseif ($target -eq "PostBuild") {
+        $manifest.Package.Identity.Name="Notepads"
+        $manifest.Package.Applications.Application.VisualElements.DisplayName="Notepads"
+        $manifest.Package.Applications.Application.Extensions.Extension.AppExecutionAlias.ExecutionAlias.Alias="Notepads.exe"
+    }
+} elseif ($config -eq "Production") {
+    if ($target -eq "PreBuild") {
+        $manifest.Package.Identity.Name="Notepads"
+        $manifest.Package.Applications.Application.VisualElements.DisplayName="Notepads"
+        $manifest.Package.Applications.Application.Extensions.Extension.AppExecutionAlias.ExecutionAlias.Alias="Notepads.exe"
+    } elseif ($target -eq "PostBuild") {
+        $manifest.Package.Identity.Name="Notepads-Dev"
+        $manifest.Package.Applications.Application.VisualElements.DisplayName="Notepads-Dev"
+        $manifest.Package.Applications.Application.Extensions.Extension.AppExecutionAlias.ExecutionAlias.Alias="Notepads-Dev.exe"
+    }
 }
+
 $manifest.Save("${manifest_file}")
