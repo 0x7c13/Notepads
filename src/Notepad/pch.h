2019675b7c897f110408883d2e2b854138d6e883
Soumya Ranjan Mahunt <devsoumyamahunt@gmail.com>
2021-01-15 17:18:02 +0530

    ImplementedWin32 component to redirect activation to UWP app.

---
+++ src/Notepad/pch.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#pragma comment(lib, "Shell32")
+#define STRICT
+#define STRICT_TYPED_ITEMIDS
+#include <windows.h>
+#include <shellapi.h>
+#include <shlobj_core.h>
+#include <winrt/base.h>
