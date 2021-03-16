a2a1cc7785088d8f29e23dac687ecec27ba4e7de
Soumya Ranjan Mahunt <devsoumyamahunt@gmail.com>
2021-03-16 10:49:32 +0530

    Implemented options to copy dektop component path/file.

--- src/Notepads/Views/Settings/AboutPage.xaml.cs
+++ src/Notepads/Views/Settings/AboutPage.xaml.cs
@@ -1,9 +1,13 @@
 ﻿namespace Notepads.Views.Settings
 {
     using System;
+    using System.Collections.Generic;
+    using System.IO;
     using Notepads.Extensions;
     using Notepads.Services;
     using Windows.ApplicationModel;
+    using Windows.ApplicationModel.DataTransfer;
+    using Windows.Storage;
     using Windows.UI.Xaml;
     using Windows.UI.Xaml.Controls;
     using Windows.UI.Xaml.Media.Imaging;
@@ -13,6 +17,8 @@
         public string AppName => App.ApplicationName;
 
         public string AppVersion => $"v{GetAppVersion()}";
+
+        private static string desktopActivationComponentPath = @"Resource\Notepad.exe";
 
         public AboutPage()
         {
@@ -57,6 +63,24 @@
         {
             PackageVersion version = Package.Current.Id.Version;
             return $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+        }
+
+        private void DesktopActivationCopyPath_Clicked(object sender, RoutedEventArgs e)
+        {
+            var path = Path.Combine(Package.Current.InstalledPath, desktopActivationComponentPath);
+            var dataPackage = new DataPackage();
+            dataPackage.RequestedOperation = DataPackageOperation.Copy;
+            dataPackage.SetText(path);
+            Clipboard.SetContent(dataPackage);
+        }
+
+        private async void DesktopActivationCopyFile_Clicked(object sender, RoutedEventArgs e)
+        {
+            var file = await Package.Current.InstalledLocation.GetFileAsync(desktopActivationComponentPath);
+            var dataPackage = new DataPackage();
+            dataPackage.RequestedOperation = DataPackageOperation.Copy;
+            dataPackage.SetStorageItems(new List<IStorageItem> { file }, true);
+            Clipboard.SetContent(dataPackage);
         }
     }
 }
