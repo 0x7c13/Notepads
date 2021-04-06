4624dc376e2c811b20c1910aaff3af48db2132d1
Soumya Ranjan Mahunt <devsoumyamahunt@gmail.com>
2021-04-06 18:34:48 +0530

    Code refactor.

--- src/Notepad/main.cpp
+++ src/Notepad/main.cpp
@@ -1,5 +1,5 @@
 ﻿#pragma once
-#pragma comment(lib, "Shell32")
+#pragma comment(lib, "shell32")
 #define STRICT
 #define STRICT_TYPED_ITEMIDS
 #include "iostream"
@@ -9,73 +9,80 @@
 #include "winrt/base.h"
 
 #ifdef _PRODUCTION
-#define AUMID L"19282JackieLiu.Notepads-Beta_echhpq9pdbte8!App"
+#define AUMID                               L"19282JackieLiu.Notepads-Beta_echhpq9pdbte8!App"
 #else
-#define AUMID L"Notepads-Dev_echhpq9pdbte8!App"
+#define AUMID                               L"Notepads-Dev_echhpq9pdbte8!App"
 #endif
 
-#define OPEN L"open"
-#define PRINT L"print"
+#define OPEN                                L"open"
+#define PRINT                               L"print"
 
-#define NOTEPADPRINTCOMMAND L"/p"
-#define NOTEPADOPENWITHANSIENCODINGCOMMAND L"/a"
+#define NOTEPADPRINTCOMMAND                 L"/p"
+#define NOTEPADOPENWITHANSIENCODINGCOMMAND  L"/a"
 #define NOTEPADOPENWITHUTF16ENCODINGCOMMAND L"/w"
+
+namespace winrt
+{
+    template <typename T, typename F, typename...Args>
+    impl::com_ref<T> parse(F function, Args&& ...args)
+    {
+        T* result{};
+        check_hresult(function(args..., &result));
+        return { result, take_ownership_from_abi };
+    }
+}
 
 using namespace std;
 using namespace winrt;
 
 #ifndef _DEBUG
-INT APIENTRY wWinMain(_In_ HINSTANCE /* hInstance */, _In_opt_ HINSTANCE /* hPrevInstance */, _In_ LPWSTR /* lpCmdLine */, _In_ INT /* nCmdShow */)
+INT APIENTRY wWinMain(
+    _In_     HINSTANCE /* hInstance */,
+    _In_opt_ HINSTANCE /* hPrevInstance */,
+    _In_     LPWSTR    /* lpCmdLine */,
+    _In_     INT       /* nCmdShow */)
 #else
 INT main()
 #endif
 {
     init_apartment();
     
-    LPWSTR* szArglist = NULL;
-    INT nArgs;
-    szArglist = CommandLineToArgvW(GetCommandLine(), &nArgs);
+    auto nArgs = 0;
+    auto szArglist = CommandLineToArgvW(GetCommandLine(), &nArgs);
     if (szArglist && nArgs > 1)
     {
-        LPCTSTR verb = OPEN;
+        auto verb = OPEN;
         if (wcscmp(NOTEPADPRINTCOMMAND, szArglist[1]) == 0) verb = PRINT;
-        bool isOpenRequested = (wcscmp(OPEN, verb) == 0);
+        auto increment = wcscmp(OPEN, verb) == 0 ? 1 : 2;
 
-        INT ct = nArgs - isOpenRequested ? 1 : 2;
-        HRESULT hr = E_OUTOFMEMORY;
-        com_ptr<IShellItemArray> ppsia = NULL;
-        PIDLIST_ABSOLUTE* rgpidl = new(std::nothrow) PIDLIST_ABSOLUTE[ct];
+        auto ct = nArgs - increment;
+        auto rgpidl = new(std::nothrow) PIDLIST_ABSOLUTE[ct];
         if (rgpidl)
         {
-            hr = S_OK;
-            INT cpidl;
-            for (cpidl = 0; SUCCEEDED(hr) && cpidl < ct; cpidl++)
+            auto cpidl = 0;
+            for (auto hr = S_OK; SUCCEEDED(hr) && cpidl < ct; cpidl++)
             {
-                hr = SHParseDisplayName(szArglist[cpidl + isOpenRequested ? 1 : 2], NULL, &rgpidl[cpidl], 0, NULL);
+                hr = SHParseDisplayName(szArglist[cpidl + increment], NULL, &rgpidl[cpidl], 0, NULL);
             }
 
-            if (cpidl > 0 && SUCCEEDED(SHCreateShellItemArrayFromIDLists(cpidl, rgpidl, ppsia.put())))
+            if (cpidl > 0)
             {
-                com_ptr<IApplicationActivationManager> appActivationMgr = NULL;
-                if (SUCCEEDED(CoCreateInstance(CLSID_ApplicationActivationManager, NULL, CLSCTX_LOCAL_SERVER, __uuidof(appActivationMgr), appActivationMgr.put_void())))
-                {
-                    DWORD pid = 0;
-                    appActivationMgr->ActivateForFile(AUMID, ppsia.get(), verb, &pid);
+                auto pid = DWORD(0);
+                auto ppsia = parse<IShellItemArray>(SHCreateShellItemArrayFromIDLists, cpidl, rgpidl);
+                auto appActivationMgr = create_instance<IApplicationActivationManager>(CLSID_ApplicationActivationManager);
+                appActivationMgr->ActivateForFile(AUMID, ppsia.get(), verb, &pid);
 #ifdef _DEBUG
-                    cout << "Launched files with process id: " << pid;
-                    Sleep(2000);
+                cout << "Launched files with process id: " << pid;
+                Sleep(2000);
 #endif
-                }
-                appActivationMgr.~com_ptr();
             }
 
-            for (INT i = 0; i < cpidl; i++)
+            for (auto i = 0; i < cpidl; i++)
             {
                 CoTaskMemFree(rgpidl[i]);
             }
         }
 
-        ppsia.~com_ptr();
         delete[] rgpidl;
     }
 
