f847e8bb3e5bfd10df8554795a4f965ffa125c7e
Soumya Ranjan Mahunt <devsoumyamahunt@gmail.com>
2021-04-17 22:38:55 +0530

    ci: setup vstest in pipeline

--- src/Notepads.UnitTest/UnitTestApp.xaml.cs
+++ src/Notepads.UnitTest/UnitTestApp.xaml.cs
@@ -29,7 +29,6 @@
         /// <param name="e">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
-
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
