39971a8965780c3541a5732c3592ba2310cbebd3
aayushpandya <50677518+aayushpandya@users.noreply.github.com>
2023-04-06 22:34:36 -0300

    removed commented code

--- src/Notepads/Utilities/SessionUtility.cs
+++ src/Notepads/Utilities/SessionUtility.cs
@@ -45,17 +45,6 @@
             return sessionManager;
         }
 
-        //public static async Task<StorageFolder> GetBackupFolderAsync(string backupFolderName)
-        //{
-        //    return await FileSystemUtility.GetOrCreateAppFolder(backupFolderName);
-        //}
-
-        //public static async Task<IReadOnlyList<StorageFile>> GetAllBackupFilesAsync(string backupFolderName)
-        //{
-        //    StorageFolder backupFolder = await GetBackupFolderAsync(backupFolderName);
-        //    return await backupFolder.GetFilesAsync();
-        //}
-
         public static async Task<string> GetSerializedSessionMetaDataAsync(string sessionMetaDataFileName)
         {
             try
@@ -107,12 +96,6 @@
                 await sessionDataFile.DeleteAsync();
             }
         }
-
-        //public static async Task<StorageFile> CreateNewFileInBackupFolderAsync(string fileName, CreationCollisionOption collisionOption, string backupFolderName)
-        //{
-        //    StorageFolder backupFolder = await GetBackupFolderAsync(backupFolderName);
-        //    return await backupFolder.CreateFileAsync(fileName, collisionOption);
-        //}
     }
 
     internal static class BackupSessionUtility
@@ -127,6 +110,7 @@
             StorageFolder backupFolder = await GetBackupFolderAsync(backupFolderName);
             return await backupFolder.GetFilesAsync();
         }
+      
         public static async Task<StorageFile> CreateNewFileInBackupFolderAsync(string fileName, CreationCollisionOption collisionOption, string backupFolderName)
         {
             StorageFolder backupFolder = await GetBackupFolderAsync(backupFolderName);
