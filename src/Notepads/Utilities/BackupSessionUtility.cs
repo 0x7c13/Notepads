e0c5595d6e7f2d5efe2ee50e5b19b01505937b0e
aayushpandya <50677518+aayushpandya@users.noreply.github.com>
2023-04-06 22:46:00 -0300

    Solved CodeFactor issue

---
+++ src/Notepads/Utilities/BackupSessionUtility.cs
@@ -0,0 +1,32 @@
+﻿namespace Notepads.Utilities
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Microsoft.Toolkit.Uwp.Helpers;
+    using Notepads.Core;
+    using Notepads.Services;
+    using Windows.Storage;
+    using Microsoft.AppCenter.Analytics;
+    
+    internal static class BackupSessionUtility
+    {
+        public static async Task<StorageFolder> GetBackupFolderAsync(string backupFolderName)
+        {
+            return await FileSystemUtility.GetOrCreateAppFolder(backupFolderName);
+        }
+
+        public static async Task<IReadOnlyList<StorageFile>> GetAllBackupFilesAsync(string backupFolderName)
+        {
+            StorageFolder backupFolder = await GetBackupFolderAsync(backupFolderName);
+            return await backupFolder.GetFilesAsync();
+        }
+      
+        public static async Task<StorageFile> CreateNewFileInBackupFolderAsync(string fileName, CreationCollisionOption collisionOption, string backupFolderName)
+        {
+            StorageFolder backupFolder = await GetBackupFolderAsync(backupFolderName);
+            return await backupFolder.CreateFileAsync(fileName, collisionOption);
+        }
+    }
+}
--- src/Notepads/Utilities/SessionUtility.cs
+++ src/Notepads/Utilities/SessionUtility.cs
@@ -97,24 +97,4 @@
             }
         }
     }
-
-    internal static class BackupSessionUtility
-    {
-        public static async Task<StorageFolder> GetBackupFolderAsync(string backupFolderName)
-        {
-            return await FileSystemUtility.GetOrCreateAppFolder(backupFolderName);
-        }
-
-        public static async Task<IReadOnlyList<StorageFile>> GetAllBackupFilesAsync(string backupFolderName)
-        {
-            StorageFolder backupFolder = await GetBackupFolderAsync(backupFolderName);
-            return await backupFolder.GetFilesAsync();
-        }
-      
-        public static async Task<StorageFile> CreateNewFileInBackupFolderAsync(string fileName, CreationCollisionOption collisionOption, string backupFolderName)
-        {
-            StorageFolder backupFolder = await GetBackupFolderAsync(backupFolderName);
-            return await backupFolder.CreateFileAsync(fileName, collisionOption);
-        }
-    }
 }
